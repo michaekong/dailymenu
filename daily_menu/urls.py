52e6950ba262632e05828379733d0df49cb0782c
michaekong <michaelndekebai@gmail.com>
2025-06-04 01:32:45 +0100

    first_url

--- daily_menu/urls.py
+++ daily_menu/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', api.urls),
-    path('start/', first, name='first'),
+    path('', first, name='first'),
     path('dashboard/', dashboard, name='dashboard'),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
     static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
